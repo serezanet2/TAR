eaf36ac1892d36ff5f96005ddaac9a64b170dda1
serezanet2 <serezanet2@gmail.com>
2026-06-28 15:35:38 +0300

    Update TAR.lua

--- TAR.lua
+++ TAR.lua
@@ -1,4 +1,4 @@
--- LocalScript (Client)
+-- LocalScript (Cвафафlient)
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
